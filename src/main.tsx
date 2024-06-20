19239c45aa122955d14e0bb9bf68eee28e9cc1c0
torres76ru <torres76ru@gmail.com>
2024-06-20 17:15:52 +0300

    added approot

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./utils/router";
 import { SDKProvider } from "@tma.js/sdk-react";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SDKProvider acceptCustomStyles debug>
-      <RouterProvider router={router} />
+      <AppRoot>
+        <RouterProvider router={router} />
+      </AppRoot>
     </SDKProvider>
   </React.StrictMode>
 );
