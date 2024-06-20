ecce122d071293ce1435c4cb86513fc3b4ccae12
torres76ru <torres76ru@gmail.com>
2024-06-20 08:19:53 +0300

    import fixes

--- src/pages/DailyReports.tsx
+++ src/pages/DailyReports.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const DailyReports = () => {
   return <div>DailyReports</div>;
 };
--- src/pages/NewReport.tsx
+++ src/pages/NewReport.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const NewReport = () => {
   return <div>NewReport</div>;
 };
